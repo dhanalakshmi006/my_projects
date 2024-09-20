50e3dcdf7480c11f4ffd6a8cbe4cce83d3c760e3
dhanalakshmi006 <99552841+dhanalakshmi006@users.noreply.github.com>
2024-09-20 18:29:33 +0530

    Delete Basics of Python directory

--- Basics of Python/Calculate area of rectangle.py
+++
@@ -1,4 +0,0 @@
-width=float(input("enter width (in centimetre) : "))
-length=float(input("enter length (in centimeter) : "))
-area=length*width
-print(area,"cm^2")
--- Basics of Python/Calculate speed.py
+++
@@ -1,4 +0,0 @@
-distance=float(input("enter distance travelled (in kilometres) : "))
-time=float(input("enter time taken (in hours) : "))
-speed=distance/time
-print(speed,"km/h")
