10cb5d1a70882bcc6662bc3b8f012177198d6d0d
dhanalakshmi006 <99552841+dhanalakshmi006@users.noreply.github.com>
2024-09-20 18:37:55 +0530

    Update Calculate Simple Interest.py

--- Basics of Python - Oct 2021/Calculate Simple Interest.py
+++ Basics of Python - Oct 2021/Calculate Simple Interest.py
@@ -1,1 +1,5 @@
-
+P = float(input("enter principal amount: "))
+R = float(input("enter rate of interest: "))
+Y = float(input("enter No. of years: "))
+Simple Interest = P*R*Y/100
+print(Simple Interest,"%")
